8a273a1061e43a9c512c1f1cb0208918b7458ad5
arsystemstarlink-oss <arsystemstarlink-oss@users.noreply.github.com>
2026-08-13 04:37:26 +0000

    deploy: febd9ba1b9e8250bba38cbd8502663d5248c529c

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/admin-suscriptions-frontend/sw.js', { scope: '/admin-suscriptions-frontend/' })})}
